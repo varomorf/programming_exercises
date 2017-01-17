50c16d27d9c60944e2bb710aa5221900ce2a395a
varomorf <varomorf@gmail.com>
2017-01-17 17:29:33 +0100

    Add partially and port Burger class.

---
+++ free/libgdx/courseraGameProgramingPort/core/src/com/platypusit/libgdx/courseraGameProgPort/Burger.java
@@ -0,0 +1,64 @@
+package com.platypusit.libgdx.courseraGameProgPort;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ * A burger.
+ *
+ * Created by alfergon on 17/01/17.
+ */
+public class Burger {
+
+    public static final String TEXTURE_PATH = "graphics/burger.png";
+
+    private static final ProjectileType PROJECTILE_TYPE = ProjectileType.FRENCH_FRIES;
+
+    // graphic and drawing info
+    Texture texture;
+    Rectangle drawRectangle;
+
+    // burger stats
+    int health = 100;
+
+    // shooting support
+    boolean canShoot = true;
+    int elapsedCooldownMilliseconds = 0;
+
+    // sound effect TODO
+    //SoundEffect shootSound;
+
+    /**
+     * <p>Creates a new Burger specifying its texture and position.</p>
+     *
+     * TODO sound
+     * @param texture The burger's texture.
+     * @param x The x position.
+     * @param y The y position.
+     */
+    public Burger(Texture texture, int x, int y) {
+        this.texture = texture;
+
+        drawRectangle = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
+    }
+
+    public Rectangle getCollisionRectangle(){
+        return drawRectangle;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        if(health < 0){
+            this.health = 0;
+        }
+
+        this.health = health;
+    }
+
+    public void update(){
+
+    }
+}
