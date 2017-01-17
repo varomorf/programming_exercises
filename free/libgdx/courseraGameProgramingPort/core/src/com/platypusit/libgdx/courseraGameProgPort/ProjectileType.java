08fdb480cc1ebff94e11a8b31900fbb79d18973b
varomorf <varomorf@gmail.com>
2017-01-17 15:50:10 +0100

    Add and port enum.

---
+++ free/libgdx/courseraGameProgramingPort/core/src/com/platypusit/libgdx/courseraGameProgPort/ProjectileType.java
@@ -0,0 +1,10 @@
+package com.platypusit.libgdx.courseraGameProgPort;
+
+/**
+ * An enumeration of the projectile types.
+ *
+ * Created by alfergon on 17/01/17.
+ */
+public enum ProjectileType {
+    FRENCH_FRIES, TEDDY_BEAR
+}
