ea71d56edcd64b6ae98d4eb4e719f3d378f6c5d9
varomorf <varomorf@gmail.com>
2017-01-17 17:29:07 +0100

    Change resolution and title.

--- free/libgdx/courseraGameProgramingPort/desktop/src/com/platypusit/libgdx/courseraGameProgPort/desktop/DesktopLauncher.java
+++ free/libgdx/courseraGameProgramingPort/desktop/src/com/platypusit/libgdx/courseraGameProgPort/desktop/DesktopLauncher.java
@@ -3,10 +3,16 @@ package com.platypusit.libgdx.courseraGameProgPort.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.platypusit.libgdx.courseraGameProgPort.CourseraGameProgramingPort;
+import com.platypusit.libgdx.courseraGameProgPort.GameConstants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Teddy bears defend themselves from an evil burger";
+		config.width = GameConstants.WINDOW_WIDTH;
+		config.height = GameConstants.WINDOW_HEIGHT;
+		config.fullscreen = false;
+
 		new LwjglApplication(new CourseraGameProgramingPort(), config);
 	}
 }
