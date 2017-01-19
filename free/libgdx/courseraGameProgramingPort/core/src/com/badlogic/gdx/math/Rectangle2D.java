b05782ce8ac6ae02df1059a69468ece378562c8a
varomorf <varomorf@gmail.com>
2017-01-19 16:36:23 +0100

    Add a port of Monogame's Rectangle2D class.

---
+++ free/libgdx/courseraGameProgramingPort/core/src/com/badlogic/gdx/math/Rectangle2D.java
@@ -0,0 +1,117 @@
+package com.badlogic.gdx.math;
+
+/**
+ * <p>Liggdx adaptation of MonoGame's Rectangle2D.</p>
+ * Created by alfergon on 19/01/17.
+ */
+public class Rectangle2D extends Rectangle {
+
+    /**
+     * Constructs a new rectangle with all values set to zero
+     */
+    public Rectangle2D() {
+        super();
+    }
+
+    /**
+     * Constructs a new rectangle with the given corner point in the bottom left and dimensions.
+     *
+     * @param x      The corner point x-coordinate
+     * @param y      The corner point y-coordinate
+     * @param width  The width
+     * @param height The height
+     */
+    public Rectangle2D(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+
+    /**
+     * Constructs a rectangle based on the given rectangle
+     *
+     * @param rect The rectangle
+     */
+    public Rectangle2D(Rectangle rect) {
+        super(rect);
+    }
+
+    /**
+     * Returns the bottommost coordinate of the rectangle.
+     *
+     * @return The bottommost coordinate of the rectangle.
+     */
+    public float getBottom() {
+        return y;
+    }
+
+    /**
+     * Sets the Y coordinate so that the bottom of the rectangle is at the given value.
+     *
+     * @param y The value for the bottom.
+     */
+    public void setBottom(float y) {
+        this.y = y;
+    }
+
+    /**
+     * Returns the topmost coordinate of the rectangle.
+     *
+     * @return The topmost coordinate of the rectangle.
+     */
+    public float getTop() {
+        return y + height;
+    }
+
+    /**
+     * Sets the Y coordinate so that the top of the rectangle is at the given value.
+     *
+     * @param y The value for the top.
+     */
+    public void setTop(float y) {
+        this.y = y - height;
+    }
+
+    /**
+     * Returns the rightmost coordinate of the rectangle.
+     *
+     * @return The rightmost coordinate of the rectangle.
+     */
+    public float getRight() {
+        return x + width;
+    }
+
+    /**
+     * Sets the X coordinate so that the right of the rectangle is at the given value.
+     *
+     * @param x The value for the right.
+     */
+    public void setRight(float x) {
+        this.x = x - width;
+    }
+
+    /**
+     * Returns the leftmost coordinate of the rectangle.
+     *
+     * @return The leftmost coordinate of the rectangle.
+     */
+    public float getLeft() {
+        return x;
+    }
+
+    /**
+     * Sets the X coordinate so that the left of the rectangle is at the given value.
+     *
+     * @param x The value for the left.
+     */
+    public void setLeft(float x) {
+        this.x = x;
+    }
+
+    /**
+     * Calculates the center of the rectangle.
+     *
+     * @return A vector with the center position.
+     */
+    public Vector2 getCenter() {
+        return getCenter(new Vector2());
+    }
+}
---
+++ free/libgdx/courseraGameProgramingPort/core/src/test/java/com/badlogic/gdx/math/Rectangle2DTest.java
@@ -0,0 +1,141 @@
+package com.badlogic.gdx.math;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Rectangle2DTest {
+
+    Rectangle2D rectangle;
+    Rectangle2D rectangle2;
+
+    @Before
+    public void setup(){
+        /*
+        * 6 | * * * *
+        *
+        * 5 | *     *
+        *
+        * 4 | *  C  *
+        *
+        * 3 | *     *
+        *
+        * 2 | * * * *
+        *
+        * 1 |
+        *
+        * 0 + - - - - - - - - - -
+        *   0 1 2 3 4 5 6 7 8 9 X
+        * */
+        rectangle = new Rectangle2D(1, 2, 3, 4);
+        /*
+        * 6 |
+        *
+        * 5 |
+        *
+        * 4 |
+        *
+        * 3 |
+        *
+        * 2 | * *
+        *      C
+        * 1 | * *
+        *
+        * 0 + - - - - - - - - - -
+        *   0 1 2 3 4 5 6 7 8 9 X
+        * */
+        rectangle2 = new Rectangle2D(1, 1, 1, 1);
+    }
+
+    @Test
+    public void givenARectangle_whenGettingItsSides_returnCorrectValues() throws Exception {
+        //GIVEN default rectangle on 1,2 with width 3 and height 4
+
+        //EXPECT
+        assertEquals("Wrong bottom value", 2, rectangle.getBottom(), 0);
+        assertEquals("Wrong top value", 6, rectangle.getTop(), 0);
+        assertEquals("Wrong left value", 1, rectangle.getLeft(), 0);
+        assertEquals("Wrong right value", 4, rectangle.getRight(), 0);
+    }
+
+    @Test
+    public void givenARectangle_whenSettingItsSides_setCorrectValues() throws Exception {
+        //GIVEN default rectangle on 1,2 with width 3 and height 4
+
+        //WHEN
+        rectangle.setLeft(5);
+
+        //THEN
+        assertEquals("Wrong left value", 5, rectangle.getLeft(), 0);
+        assertEquals("Wrong X value", 5, rectangle.x, 0);
+        assertEquals("Moved Y coordinate when not necessary", 2, rectangle.y, 0);
+
+        //WHEN
+        rectangle.setRight(5);
+
+        //THEN
+        assertEquals("Wrong right value", 5, rectangle.getRight(), 0);
+        assertEquals("Wrong X value", 2, rectangle.x, 0);
+        assertEquals("Moved Y coordinate when not necessary", 2, rectangle.y, 0);
+
+        //WHEN
+        rectangle.setTop(5);
+
+        //THEN
+        assertEquals("Wrong top value", 5, rectangle.getTop(), 0);
+        assertEquals("Moved X coordinate when not necessary", 2, rectangle.x, 0);
+        assertEquals("Wrong Y value", 1, rectangle.y, 0);
+
+        //WHEN
+        rectangle.setBottom(5);
+
+        //THEN
+        assertEquals("Wrong bottom value", 5, rectangle.getBottom(), 0);
+        assertEquals("Moved X coordinate when not necessary", 2, rectangle.x, 0);
+        assertEquals("Wrong Y value", 5, rectangle.y, 0);
+    }
+
+    @Test
+    public void givenARectangle_whenGettingCenterWithoutVector_returnCenterCorrectly() throws Exception {
+        //GIVEN default rectangle on 1,2 with width 3 and height 4
+
+        //WHEN
+        Vector2 center = rectangle.getCenter();
+
+        //THEN
+        assertEquals("Wrong X value", 2.5, center.x, 0);
+        assertEquals("Wrong Y value", 4, center.y, 0);
+
+        //GIVEN default rectangle on 1,1 with width 1 and height 1
+
+        //WHEN
+        Vector2 center2 = rectangle2.getCenter();
+
+        //THEN
+        assertEquals("Wrong X value", 1.5, center2.x, 0);
+        assertEquals("Wrong Y value", 1.5, center2.y, 0);
+    }
+
+    @Test
+    public void givenARectangle_whenSettingCenter_setCorrectValues() throws Exception {
+        //GIVEN default rectangle on 1,2 with width 3 and height 4
+
+        //WHEN
+        rectangle.setCenter(5, 5);
+
+        //THEN
+        assertEquals("Wrong X value", 3.5, rectangle.x, 0);
+        assertEquals("Wrong Y value", 3, rectangle.y, 0);
+
+        //GIVEN default rectangle on 1,1 with width 1 and height 1
+
+        //WHEN
+        rectangle2.setCenter(5, 5);
+
+        //THEN
+        assertEquals("Wrong X value", 4.5, rectangle2.x, 0);
+        assertEquals("Wrong Y value", 4.5, rectangle2.y, 0);
+    }
+
+}
